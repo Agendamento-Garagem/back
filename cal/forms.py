86e4a65d31f3e445e93ed79492d500bc9f55fbd7
Rafael Falk <rafaelmartinsfalk2@gmail.com>
2022-11-06 22:58:21 -0300

    adding error messages

--- cal/forms.py
+++ cal/forms.py
@@ -1,8 +1,9 @@
 from csv import field_size_limit
 from django.forms import ModelForm, DateInput
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth.models import User
 from cal.models import Event, Reason
+from django import forms
 
 class EventForm(ModelForm):
   class Meta:
@@ -24,6 +25,35 @@ class CreateFormUser(UserCreationForm):
   class Meta:
     model = User
     fields = ['username', 'email', 'password1', 'password2']
+  def clean_username(self):
+        username = self.cleaned_data.get('username')
+        model = self.Meta.model
+        user = model.objects.filter(username__iexact=username)
+        
+        if user.exists():
+            raise forms.ValidationError("Já existe um usuário com esse nome!")
+        
+        return self.cleaned_data.get('username')
+
+  def clean_email(self):
+      email = self.cleaned_data.get('email')
+      model = self.Meta.model
+      user = model.objects.filter(email__iexact=email)
+      
+      if user.exists():
+          raise forms.ValidationError("Já existe um usuário com esse email!")
+      
+      return self.cleaned_data.get('email')
+
+
+  def clean_password(self):
+      password = self.cleaned_data.get('password1')
+      password2 = self.data.get('password2')
+      
+      if password != password2:
+          raise forms.ValidationError("Senhas diferentes!")
+
+      return self.cleaned_data.get('password1')
   
 class ReasonForm(ModelForm):
   class Meta:
